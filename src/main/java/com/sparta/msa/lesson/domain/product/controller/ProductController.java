8c670be72524bbe826e515141f49d71a05c1776e
Youjaeseon <wotjd0701@naver.com>
2026-03-16 22:42:22 +0900

    Controller 세팅

--- src/main/java/com/sparta/msa/lesson/domain/product/controller/ProductController.java
+++ src/main/java/com/sparta/msa/lesson/domain/product/controller/ProductController.java
@@ -18,32 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController// API 요청을 가장 먼저 받는 클래스로 인식 -> API 스펙이 정의된 곳.
 @RequiredArgsConstructor
-@RequestMapping("/api/products") // API 매핑 지정
+@RequestMapping("api/products") // API 매핑 지정
 public class ProductController {
 
   private final ProductService productService;
 
   // API [GET] /api/products?name="전화기"   // HTTP 상태 코드를 지정하지 않으면 = > 200
-  @GetMapping("/api/products/{id}")
-  public List<Product> searchProducts(
-      @RequestParam(name = "name") String name) { // API [GET] /api/products/{id}
+  @GetMapping("/{id}")
+  public List<Product> searchProducts(@RequestParam(name = "name") String name) {
     return List.of();
 
   }
 
-  @PostMapping("/api/proudcts")
+  // API [GET] /api/products/1
+  @GetMapping("/{id}")
+  public Product findById(@PathVariable Long id) {
+    return Product.builder().build();
+  }
+
+  @PostMapping
   @ResponseStatus(HttpStatus.CREATED) // <- HTTP 상태코드 지정이 가능하다.
   public void create(@RequestBody Product product) {
 
     return;
   }
 
-  @PutMapping("/api/products")
-  public Product update(@RequestBody Product product) {
+  @PutMapping
+  public void update(@RequestBody Product product) {
     return;
   }
 
-  @DeleteMapping("/api/products/{id}")
+  @DeleteMapping("/{id}")
   public void delete(@PathVariable Long id) {
     return;
   }
