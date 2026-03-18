3daa833cc14094e3222a15dabca2c7116454a80f
Youjaeseon <wotjd0701@naver.com>
2026-03-18 21:56:13 +0900

    매퍼 변경

--- src/main/java/com/sparta/msa/lesson/domain/user/entity/User.java
+++ src/main/java/com/sparta/msa/lesson/domain/user/entity/User.java
@@ -22,61 +22,49 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.util.StringUtils;
 
-@Entity // 이 클래스는 JPA가 관리하는 엔티티입니다.
-@Getter // Lombok: Getter 메서드를 자동 생성합니다.
-@DynamicInsert // 값이 null이 아닌 필드만으로 INSERT 쿼리를 생성합니다.
-@DynamicUpdate // 변경된 필드만으로 UPDATE 쿼리를 생성합니다.
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA는 기본 생성자를 필요로 합니다.
-@Table(name = "users") // 'users'라는 이름의 테이블과 매핑됩니다.
+@Table(name = "users")
 public class User {
 
-  @Id // 이 필드가 테이블의 Prima ry Key(기본 키)입니다.
-  @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성을 DB의 AUTO_INCREMENT에 위임합니다.
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   Long id;
 
-  @Column(nullable = false, length = 50) // 'username' 컬럼, NOT NULL, 길이 50
+  @Column(nullable = false, length = 50)
   String name;
 
-  @Column(nullable = false, unique = true) // 'email' 컬럼, NOT NULL, UNIQUE
+  @Column(nullable = false, unique = true)
   String email;
 
   @Column(nullable = false)
   String password;
 
-  @CreationTimestamp // 엔티티가 생성될 때의 시간이 자동으로 기록됩니다.
+  @CreationTimestamp
   @Column(nullable = false, updatable = false)
   LocalDateTime createdAt;
 
-  @UpdateTimestamp // 엔티티가 수정될 때의 시간이 자동으로 기록됩니다.
-  @Column
+  @UpdateTimestamp
+  @Column(nullable = false)
   LocalDateTime updatedAt;
 
-  @BatchSize(size = 20)
-  @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+  @BatchSize(size = 100)
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
   private List<Order> orders = new ArrayList<>();
 
-
-  @Builder // 빌더 패턴으로 객체를 생성할 수 있게 합니다.
-  public User(String name, String email, String password) {
+  @Builder
+  public User(
+      String name,
+      String email,
+      String password
+  ) {
     this.name = name;
     this.email = email;
-    this.password = password;
-  }
-
-  public void setName(String name) {
-    if (StringUtils.hasText(name)) {
-      this.name = name;
-    }
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setPassword(String password) {
     this.password = password;
   }
 }
